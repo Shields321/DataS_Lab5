a469eb5f81e937806ed779d8a7c39542ce8fa135
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-10 17:35:25 -0400

    Update DataS_Lab5.java

--- datas_lab5/DataS_Lab5.java
+++ datas_lab5/DataS_Lab5.java
@@ -1,9 +1,23 @@
-package datas_lab5;
+package DataS_Lab5;
 
 public class DataS_Lab5 {
-
     public static void main(String[] args) {
-        LinearHashTable map = new LinearHashTable();
+        Machine m = new Machine("Aleks", "Dylan", "YOUR MOM");
+        Machine m2 = new Machine("Alessio", "Dylan", "YOUR MOM");
         
+        
+        
+        LinearHashTable map2 = new LinearHashTable(10, 0.7);
+        QuadHashTable map = new QuadHashTable(10, 0.7);
+
+        map.add(m);
+        map2.add(m2);
+       
+
+
+        System.out.println("Linear\n"+map.getLocation(m.getMachineCode()));
+        System.out.println("-----------------------");
+        System.out.println("Quad\n"+map2.getLocation(m2.getMachineCode()));
+
     }
 }
