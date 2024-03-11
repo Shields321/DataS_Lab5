cb681715f0622a7cb5c10e04e1ff8292ba56100d
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-11 16:14:43 -0400

    Update LinearHashTable.java

--- datas_lab5/LinearHashTable.java
+++ datas_lab5/LinearHashTable.java
@@ -1,6 +1,6 @@
-package DataS_Lab5;
+package datas_lab5;
 
-/*public class LinearHashTable {
+public class LinearHashTable {
     private final static int MAXIMUM_CAPACITY = 1 << 30;
     private int maxSize;
     private Double loadFactor;
@@ -9,23 +9,16 @@ package DataS_Lab5;
     int numItems;
     int index = 0;
 
-
     public LinearHashTable(int max, double loadFactor) {
         if (max > MAXIMUM_CAPACITY) {
-            this.maxSize = MAXIMUM_CAPACITY;
-            Machine[] upSizedMachine = new Machine[maxSize];
-            for(int i = 0;i<maxSize;i++){
-                upSizedMachine[i] = machine[i];
-            }
+            this.maxSize = MAXIMUM_CAPACITY;            
         } else
             this.maxSize = trimToPowerOf2(max);
         this.loadFactor = loadFactor;
-        machine = new Machine[maxSize];
-        
+        machine = new Machine[maxSize];       
     }
 
-    private int trimToPowerOf2(int startingSize) {
-        
+    private int trimToPowerOf2(int startingSize) {       
         int capacity = 1;
         while (capacity < startingSize) {
             capacity <<= 1;
@@ -34,8 +27,7 @@ package DataS_Lab5;
     }
 
     public int add(Machine m) {
-        try {
-            
+        try {            
             String code = m.getMachineCode();
             int hashCode = hashFunction(code);
             int pos = hashCode % maxSize;
@@ -49,7 +41,8 @@ package DataS_Lab5;
             
             return pos;
         } catch (Exception e) {
-            maxSize = trimToPowerOf2(maxSize);
+            //System.out.println("Error: " + e); // only un comment if you want to see what error happened
+            System.out.println("Amount of spaces full please try again later (end Program)");
         }
         return 0;
     }
@@ -66,15 +59,21 @@ package DataS_Lab5;
     }
 
     public int getLocation(String key) {
-        int hashCode = hashFunction(key);
-        int pos = hashCode % maxSize;
-        if (machine[pos] == null) {
+        try{
+            int hashCode = hashFunction(key);
+            int pos = hashCode % maxSize;
+            if (machine[pos] == null) {
+                return -1;
+            }
+            while (!(machine[pos].getMachineCode().equals(key))) {
+                pos = (pos + 1) % maxSize;
+            }
+            return pos;
+        }catch(Exception e){
+            //System.out.println("Error: " + e); // only un comment if you want to see what error happened
+            System.out.println("Key Not in Machine List");
             return -1;
-        }
-        while (machine[pos].getMachineCode().equals(key)) {
-            pos = (pos + 1) % maxSize;
-        }
-        return pos;
+        } 
     }
     
 
@@ -92,7 +91,7 @@ package DataS_Lab5;
             machine[loc] = null;
             return true;
         } catch (Exception e) {
-            return false;
+            return false;   
         }
     }
 
@@ -106,106 +105,4 @@ package DataS_Lab5;
         }
         return builder.toString();
     }
-}*/
-import java.util.ArrayList;
-
-public class LinearHashTable {
-    private final static int MAXIMUM_CAPACITY = 1 << 30;
-    private int maxSize;
-    private Double loadFactor;
-    private Machine noIndexMachine = new Machine("no index", "no index", "no index");
-    private ArrayList<Machine> machineList;
-    private int numItems;
-
-    public LinearHashTable(int max, double loadFactor) {
-        if (max > MAXIMUM_CAPACITY) {
-            this.maxSize = MAXIMUM_CAPACITY;
-        } else {
-            this.maxSize = trimToPowerOf2(max);
-        }
-        this.loadFactor = loadFactor;
-        this.machineList = new ArrayList<>(maxSize);
-        for (int i = 0; i < maxSize; i++) {
-            this.machineList.add(null);
-        }
-    }
-
-    private int trimToPowerOf2(int startingSize) {
-        int capacity = 1;
-        while (capacity < startingSize) {
-            capacity <<= 1;
-        }
-        return capacity;
-    }
-
-    public int add(Machine m) {
-        try {
-            String code = m.getMachineCode();
-            int hashCode = hashFunction(code);
-            int pos = hashCode % maxSize;
-            // Linear probing to handle collisions
-            while (machineList.get(pos) != null) {
-                pos = (pos + 1) % maxSize;
-            }
-            machineList.set(pos, m);
-            numItems++;
-            return pos;
-        } catch (Exception e) {
-            maxSize = trimToPowerOf2(maxSize);
-        }
-        return 0;
-    }
-
-    public int hashFunction(String key) {
-        int hash = 0;
-        // Iterate over each character in the string
-        for (int i = 0; i < key.length(); i++) {
-            // Add the ASCII value of the character to the hash
-            hash += key.charAt(i);
-        }
-        // Take the remainder when divided by the size of the hash table
-        return hash % maxSize;
-    }
-
-    public int getLocation(String key) {
-        int hashCode = hashFunction(key);
-        int pos = hashCode % maxSize;
-        if (machineList.get(pos) == null) {
-            return -1;
-        }
-        while (!machineList.get(pos).getMachineCode().equals(key)) {
-            pos = (pos + 1) % maxSize;
-        }
-        return pos;
-    }
-
-    public Machine retrieve(String key) {
-        int pos = getLocation(key);
-        if (pos == -1) {
-            return noIndexMachine;
-        }
-        return machineList.get(pos);
-    }
-
-    public boolean delete(String key) {
-        try {
-            int loc = getLocation(key);
-            machineList.set(loc, null);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public String displayMachines() {
-        StringBuilder builder = new StringBuilder("");
-        for (int i = 0; i < maxSize; i++) {
-            if (machineList.get(i) != null) {
-                builder.append("Index ").append(i).append("\n");
-                builder.append(machineList.get(i).ToString());
-            }
-        }
-        return builder.toString();
-    }
 }
-
