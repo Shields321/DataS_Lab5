4f14a26cc96a0c9e60562a179a6a6a901d0ccbc9
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-12 19:26:41 -0400

    Update LinearHashTable.java

--- datas_lab5/LinearHashTable.java
+++ datas_lab5/LinearHashTable.java
@@ -4,47 +4,35 @@ public class LinearHashTable {
     private final static int MAXIMUM_CAPACITY = 1 << 30;
     private int maxSize;
     private Double loadFactor;
-    Machine noIndexMachine = new Machine("no index", "no index", "no index");
+    private Machine noIndexMachine = new Machine("no index", "no index", "no index");
     Machine[] machine;
     int numItems;
     int index = 0;
 
     public LinearHashTable(int max, double loadFactor) {
-        if (max > MAXIMUM_CAPACITY) {
-            this.maxSize = MAXIMUM_CAPACITY;            
-        } else
-            this.maxSize = trimToPowerOf2(max);
+        this.maxSize = max;
         this.loadFactor = loadFactor;
         machine = new Machine[maxSize];       
     }
 
-    public int trimToPowerOf2(int startingSize) {       
-        int capacity = 1;
-        while (capacity < startingSize) {
-            capacity <<= 1;
-        }
-        return capacity;
-    }
-
     public int add(Machine m) {
-        try {            
+        double loadFactor = (1.0*numItems)/maxSize;
+        if(loadFactor < this.loadFactor){
             String code = m.getMachineCode();
             int hashCode = hashFunction(code);
             int pos = hashCode % maxSize;
             // Linear probing to handle collisions
             while (machine[pos] != null) {// if pos = 15
-                pos = (pos + 1) % maxSize; // add 1 to pos --- 16 mod 16 = 0 so the value would go into the first index
-                                           // if first index is ocupied go to second
+                pos = (pos + 1) % maxSize; // add 1 to pos --- 16 mod 16 = 0 so the value would go into the first index if first index is ocupied go to second
             }
             machine[pos] = m;
             numItems++;
-            
+            //System.out.println(pos);
             return pos;
-        } catch (Exception e) {
-            //System.out.println("Error: " + e); // only un comment if you want to see what error happened
-            System.out.println("Amount of spaces full please try again later (end Program)");
-        }
-        return 0;
+        }else{
+            System.out.println("Not enough space");//exceded load Factor typically would need to resize the array after this point
+            return 0;
+        }        
     }
 
     public int hashFunction(String key) {
@@ -57,22 +45,42 @@ public class LinearHashTable {
         // Take the remainder when divided by the size of the hash table
         return hash % maxSize;
     }
-
+    
     public int getLocation(String key) {
         try{
             int hashCode = hashFunction(key);
-            int pos = hashCode % maxSize;
-            if (machine[pos] == null) {
-                return -1;
+            int pos = hashCode % maxSize;   
+            while (!(machine[pos].getMachineCode().equals(key))) {
+                pos = (pos + 1) % maxSize;
+            }                        
+            return pos;
+        }catch(Exception e){    
+            
+            //System.out.println("Error: " + j); // only un comment if you want to see what error happened
+            System.out.println("\nKey Not in Machine List");
+            return -1;
+            
+        } 
+    }
+    public int getDeleteLocation(String key) {
+        try{
+            int hashCode = hashFunction(key);
+            int pos = hashCode % maxSize;   
+            if(machine[pos] == null){
+                while(machine[pos] == null){
+                    pos++;
+                }
             }
             while (!(machine[pos].getMachineCode().equals(key))) {
                 pos = (pos + 1) % maxSize;
-            }
+            }                        
             return pos;
-        }catch(Exception e){
-            //System.out.println("Error: " + e); // only un comment if you want to see what error happened
-            System.out.println("Key Not in Machine List");
+        }catch(Exception e){    
+            
+            //System.out.println("Error: " + j); // only un comment if you want to see what error happened
+            System.out.println("\nKey Not in Machine List");
             return -1;
+            
         } 
     }
     
@@ -86,8 +94,9 @@ public class LinearHashTable {
 
     public boolean delete(String key) {
         try {
-            int loc = getLocation(key);
+            int loc = getDeleteLocation(key);                
             machine[loc] = null;
+            System.out.println("Value deleted at index:"+ loc);
             return true;
         } catch (Exception e) {
             System.out.println("No Value with the key: " + key);
@@ -99,7 +108,7 @@ public class LinearHashTable {
         StringBuilder builder = new StringBuilder("");
         for (int i = 0; i < maxSize; i++) {
             if (machine[i] != null) {
-                builder.append("Index " + i + "\n");
+                builder.append("\n\nIndex " + i + "\n");
                 builder.append(machine[i].ToString());
             }
         }
