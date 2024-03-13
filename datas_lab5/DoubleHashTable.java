a896e0eb50afff45f6b31dde1d1d998cb2f3de5d
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-13 14:45:35 -0400

    Update DoubleHashTable.java

--- datas_lab5/DoubleHashTable.java
+++ datas_lab5/DoubleHashTable.java
@@ -1,13 +1,106 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package datas_lab5;
+package DataS_Lab5;
 
-/**
- *
- * @author reald
- */
 public class DoubleHashTable {
-    
+    private int maxSize;
+    private Double loadFactor;
+    private Machine noIndexMachine = new Machine("no index", "no index", "no index");
+    Machine[] machine;
+    int numItems;
+    int index = 0;
+
+    public DoubleHashTable(int max, double loadFactor) {
+        this.maxSize = max;
+        this.loadFactor = loadFactor;
+        machine = new Machine[maxSize];
+    }
+
+    public int add(Machine m) {
+        double loadFactor = (1.0 * numItems) / maxSize;
+        
+        if (loadFactor < this.loadFactor) {
+            
+            String code = m.getMachineCode();
+            int stepSize = hashFunction2(code);
+            int pos = hashFunction(code);
+            // Linear probing to handle collisions
+            while (machine[pos] != null) {
+                pos = (pos + stepSize) % maxSize;
+            }
+            machine[pos] = m;
+            numItems++;
+            System.out.println(pos);
+            return pos;
+        } else {
+            System.out.println("Not enough space");// exceded load Factor typically would need to resize the array after                                     
+            return 0;
+        }
+    }
+
+    public int hashFunction(String key) {
+        int hash = 0;
+        // Iterate over each character in the string
+        for (int i = 0; i < key.length(); i++) {
+            // Add the ASCII value of the character to the hash
+            hash += key.charAt(i);
+        }
+        // Take the remainder when divided by the size of the hash table
+        return hash % maxSize;
+    }
+
+    public int hashFunction2(String key) {
+        int prime = 7;
+        return prime - (hashFunction(key) % prime);
+    }
+
+    public int getLocation(String key) {
+        try {
+            int stepSize = hashFunction2(key);
+            int pos = hashFunction(key);
+            
+            if (machine[pos] == null) {
+                while (machine[pos] == null) {
+                    pos++;
+                }
+            }
+            while (!(machine[pos].getMachineCode().equals(key))) {
+                pos = (pos + stepSize) % maxSize;
+            }
+            return pos;
+        } catch (Exception e) {
+            // System.out.println("Error: " + e); // only un comment if you want to see what
+            // error happened
+            System.out.println("Key Not in Machine List");
+            return -1;
+        }
+    }
+
+    public Machine retrieve(String key) {
+        int pos = getLocation(key);
+        if (pos == -1) {
+            return noIndexMachine;
+        }
+        return machine[pos];
+    }
+
+    public boolean delete(String key) {
+        try {
+            int loc = getLocation(key);
+            machine[loc] = null;
+            return true;
+        } catch (Exception e) {
+            System.out.println("No Value with the key: " + key);
+            return false;
+        }
+    }
+
+    public String displayMachines() {
+        StringBuilder builder = new StringBuilder("");
+        for (int i = 0; i < maxSize; i++) {
+            if (machine[i] != null) {
+                builder.append("Index " + i + "\n");
+                builder.append(machine[i].ToString());
+            }
+        }
+        return builder.toString();
+    }
 }
