f097a6a8d2c2e73eccec8c4e61a82012d4e0021b
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-13 14:46:37 -0400

    Update DataS_Lab5.java

--- datas_lab5/DataS_Lab5.java
+++ datas_lab5/DataS_Lab5.java
@@ -6,29 +6,35 @@ public class DataS_Lab5 {
         Machine m = new Machine("Aleks", "Dylan", "YOUR MOM");
         Machine m2 = new Machine("abc", "Dylan", "YOUR MOM");
                     
-        LinearHashTable map = new LinearHashTable(16, 0.7);
+        DoubleHashTable map = new DoubleHashTable(16, 0.7);
 
-        map.add(m2);
+        map.add(m);
         map.add(m);  
-        map.add(m2); 
-        map.add(m2);  
+        map.add(m); 
+        map.add(m);
+        map.add(m);  
+        map.add(m); 
+        map.add(m);
+        map.add(m);  
+        map.add(m); 
+        map.add(m);
+        map.add(m);  
+        map.add(m); 
+        map.add(m);
+        map.add(m);  
         map.add(m); 
         
-        System.out.println(map.displayMachines());
+        
+        
         
         System.out.println("\n");
         
-        System.out.println(map.delete("abc"));
         System.out.println(map.delete("Aleks"));
-        System.out.println(map.delete("abc"));
         System.out.println(map.delete("Aleks"));
-        System.out.println(map.delete("abc"));
-        System.out.println(map.delete("a"));
+        System.out.println(map.delete("Aleks"));
         
-        
-        System.out.println("\nAfter deletion\n");
-        
-        System.out.println(map.displayMachines());
+
         
     }
 }
+
