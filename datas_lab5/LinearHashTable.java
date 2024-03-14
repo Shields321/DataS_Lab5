5c4532603fd30f2915163620e2454b29f04045f7
LuffyPandaGreat <158603448+LuffyPandaGreat@users.noreply.github.com>
2024-03-14 14:45:52 -0400

    Update LinearHashTable.java

--- datas_lab5/LinearHashTable.java
+++ datas_lab5/LinearHashTable.java
@@ -1,8 +1,8 @@
 package datas_lab5;
 
 public class LinearHashTable {
-    private int maxSize; //used to set the max size of the array. 
-    private Double loadFactor; //
+    private int maxSize; //Used to set the max size of the array. 
+    private Double loadFactor; 
     private Machine noIndexMachine = new Machine("no index", "no index", "no index");
     Machine[] machine;
     int numItems;
@@ -10,16 +10,16 @@ public class LinearHashTable {
 
     public LinearHashTable(int max, double loadFactor) {
         this.maxSize = max;
-        this.loadFactor = loadFactor;
-        machine = new Machine[maxSize];       
+        this.loadFactor = loadFactor; //This is the load factor set by the user. 
+        machine = new Machine[maxSize]; //Creating the array that store the keys.        
     }
 
     public int add(Machine m) {
-        double loadFactor = (1.0*numItems)/maxSize;
-        if(loadFactor < this.loadFactor){
-            String code = m.getMachineCode();
+        double loadFactor = (1.0*numItems)/maxSize; //This load factor is used to check if the array is near full.
+        if(loadFactor < this.loadFactor){ //Adds the element when the calculated load factor is smaller than the default load factor. 
+            String code = m.getMachineCode(); 
             int hashCode = hashFunction(code);
-            int pos = hashCode % maxSize;
+            int pos = hashCode % maxSize; //index of where the element can be added. 
             // Linear probing to handle collisions
             while (machine[pos] != null) {// if pos = 15
                 pos = (pos + 1) % maxSize; // add 1 to pos --- 16 mod 16 = 0 so the value would go into the first index if first index is ocupied go to second
@@ -46,19 +46,19 @@ public class LinearHashTable {
     }
     
     public int getLocation(String key) {
-        try{
+        try{ 
             int hashCode = hashFunction(key);
             int pos = hashCode % maxSize;  
-            if(machine[pos] == null){
+            if(machine[pos] == null){ //This is triggered if the starting position to look for the key is null.
                 while(machine[pos] == null){
                     pos++;
                 }
             }
-            while (!(machine[pos].getMachineCode().equals(key))) {
+            while (!(machine[pos].getMachineCode().equals(key))) { //looks for the key.
                 pos = (pos + 1) % maxSize;
             }                        
             return pos;
-        }catch(Exception e){    
+        }catch(Exception e){ //This catch is triggered when the key is not in the array.  
             
             //System.out.println("Error: " + j); // only un comment if you want to see what error happened
             System.out.println("\nKey Not in Machine List");
@@ -68,8 +68,8 @@ public class LinearHashTable {
     }    
     
     public Machine retrieve(String key) {
-        int pos = getLocation(key);
-        if (pos == -1) {
+        int pos = getLocation(key); //Gets the index of the machine.
+        if (pos == -1) { //returns no index when key has no machine at that index. 
             return noIndexMachine;
         }
         return machine[pos];
@@ -77,16 +77,17 @@ public class LinearHashTable {
 
     public boolean delete(String key) {
         try {
-            int loc = getLocation(key);                
-            machine[loc] = null;
+            int loc = getLocation(key); //gets the location of the key.                
+            machine[loc] = null; //make the machine at that index null.
             System.out.println("Value deleted at index:"+ loc);
             return true;
-        } catch (Exception e) {
+        } catch (Exception e) { //Triggers when the key is not in the array.
             System.out.println("No Value with the key: " + key);
             return false;   
         }
     }
 
+    //This is used to display all the elements in the array. 
     public String displayMachines() {
         StringBuilder builder = new StringBuilder("");
         for (int i = 0; i < maxSize; i++) {
